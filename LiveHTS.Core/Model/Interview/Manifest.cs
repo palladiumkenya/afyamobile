89670a7e007544db7fb7e134a7892359661da6ed
Koske Kimutai <koskedk@live.com>
2017-07-21 00:50:05 +0300

    clean up

--- LiveHTS.Core/Model/Interview/Manifest.cs
+++ LiveHTS.Core/Model/Interview/Manifest.cs
@@ -13,21 +13,9 @@ namespace LiveHTS.Core.Model.Interview
         public List<Question> QuestionStore { get; set; } = new List<Question>();
         public List<Response> ResponseStore { get; set; } = new List<Response>();
 
-        public bool HasQuestions()
-        {
-            return null != QuestionStore && QuestionStore.Count > 0;
-        }
-
-        public bool HasResponses()
-        {
-            return null != ResponseStore && ResponseStore.Count > 0;
-        }
-       
-
         public Manifest()
         {
         }
-
         private Manifest(List<Question> questions, List<Response> responses, Encounter encounter)
         {
             QuestionStore = questions;
@@ -35,6 +23,7 @@ namespace LiveHTS.Core.Model.Interview
             Encounter = encounter;
             UpdateResponseQuestions();
         }
+
 
         public static Manifest Create(Form form, Encounter encounter)
         {
@@ -44,6 +33,15 @@ namespace LiveHTS.Core.Model.Interview
             return new Manifest(formWithQuestions.Questions, responses, encounter);
         }
 
+        public bool HasQuestions()
+        {
+            return null != QuestionStore && QuestionStore.Count > 0;
+        }
+        public bool HasResponses()
+        {
+            return null != ResponseStore && ResponseStore.Count > 0;
+        }
+
         public void UpdateEncounter(Encounter encounter)
         {
             Encounter = encounter;
@@ -51,14 +49,17 @@ namespace LiveHTS.Core.Model.Interview
             UpdateResponseQuestions();
         }
 
+        public Response GetLastResponse()
+        {
+            return HasResponses() ? ResponseStore.OrderBy(x => x.Question.Rank).LastOrDefault() : null;
+        }
         private void UpdateResponseQuestions()
         {
             foreach (var response in ResponseStore)
             {
                 response.Question = GetQuestion(response.QuestionId);
             }
-        }
-
+        }     
         private static List<Response> ReadResponses(Encounter encounter)
         {
             var answeredQuestions = new List<Response>();
@@ -78,29 +79,15 @@ namespace LiveHTS.Core.Model.Interview
             return answeredQuestions;
         }
 
-        public Question GetFirstQuestion()
-        {
-            return QuestionStore.OrderBy(x => x.Rank).FirstOrDefault();
-        }
-
-        public Response GetLastResponse()
-        {
-            return HasResponses() ? ResponseStore.OrderBy(x => x.Question.Rank).LastOrDefault() : null;
-        }
-
-        public override string ToString()
-        {
-            var stats = $"{ResponseStore.Count}/{QuestionStore.Count}";
-            var summary = Encounter.IsComplete ? " Completed" : " Open";
-            return $" Status:{stats} ,{summary}";
-        }
-
         public Question GetQuestion(Guid value)
 
         {
             return QuestionStore.FirstOrDefault(x => x.Id == value);
         }
-
+        public Question GetFirstQuestion()
+        {
+            return QuestionStore.OrderBy(x => x.Rank).FirstOrDefault();
+        }
         public Question GetNextRankQuestionAfter(Guid currentQuestionId)
         {
             var currenQuestion = GetQuestion(currentQuestionId);
@@ -112,7 +99,6 @@ namespace LiveHTS.Core.Model.Interview
                 .FirstOrDefault(x => x.Rank >= currentRank &&
                                      x.Id != currentId);
         }
-
         public Question GetPreviousRankQuestionBefore(Guid currentQuestionId)
         {
             var currenQuestion = GetQuestion(currentQuestionId);
@@ -123,6 +109,13 @@ namespace LiveHTS.Core.Model.Interview
                 .OrderBy(x=>x.Rank)
                 .FirstOrDefault(x => x.Rank <= currentRank &&
                                      x.Id != currentId);
+        }
+
+        public override string ToString()
+        {
+            var stats = $"{ResponseStore.Count}/{QuestionStore.Count}";
+            var summary = Encounter.IsComplete ? " Completed" : " Open";
+            return $" Status:{stats} ,{summary}";
         }
     }
 }
