fd40baa1a56316773d3e5e9712b13f2940c3dd6f
Koske Kimutai <koskedk@live.com>
2017-08-06 19:54:18 +0300

    lie low 2

--- LiveHTS.Presentation/Interfaces/ViewModel/IClientDashboardViewModel.cs
+++ LiveHTS.Presentation/Interfaces/ViewModel/IClientDashboardViewModel.cs
@@ -1,10 +1,13 @@
-﻿using LiveHTS.Core.Model.Subject;
+﻿using System.Collections.Generic;
+using LiveHTS.Core.Model.Subject;
+using LiveHTS.Core.Model.Survey;
 using MvvmCross.Core.ViewModels;
 
 namespace LiveHTS.Presentation.Interfaces.ViewModel
 {
     public interface IClientDashboardViewModel
     {
+        IEnumerable<Module> Modules { get; set; }
         Client Client { get; set; }
         IMvxCommand ManageRegistrationCommand { get; }
         IMvxCommand AddRelationShipCommand { get; }
@@ -12,5 +15,6 @@ namespace LiveHTS.Presentation.Interfaces.ViewModel
         Client SeletctedRelationShip { get; set; }
         bool IsBusy { get; set; }
         void ShowRegistry();
+
     }
 }
