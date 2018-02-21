f5ffdb49588fff94ac3c1fb69592cdc41fa294db
Koske Kimutai <koskedk@live.com>
2018-02-21 20:29:22 +0300

    clean up

--- LiveHTS.Presentation/ViewModel/PushDataViewModel.cs
+++ LiveHTS.Presentation/ViewModel/PushDataViewModel.cs
@@ -193,8 +193,8 @@ namespace LiveHTS.Presentation.ViewModel
                     clientIdsDelete.Add(clientToDeleteDto);
                     foreach (var toDeleteDto in clientIdsDelete)
                     {
-                        //TODO: ALLOW DELETE
-                       // _clientReaderService.Purge(toDeleteDto);
+                        //TODO: ALLOW DELETE [DONE]
+                        _clientReaderService.Purge(toDeleteDto);
                     }
                 }
                 
